4a5976a840d9297f8b18d30bc3be73af26a5d229
t00186568 <kieran.mcclure@students.ittralee.ie>
2017-11-20 16:21:54 +0000

    Created JFrame Window

---
+++ src/Connect4.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+
+public class Connect4 extends JFrame {
+     
+
+    private static void Connect4(){
+        JFrame frame = new JFrame("Connect 4");
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setSize(800,600);
+
+    }
+
+
+
+}
