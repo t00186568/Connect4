848c9504e1283cb0e04e46f2acf288d4e0da42c0
t00186568 <kieran.mcclure@students.ittralee.ie>
2017-11-20 17:32:10 +0000

    Draw Circles Loop
    Created nested for loops to draw circles using java.awt.Graphics, Circles are laid out in 7x6 grid

--- src/Connect4.java
+++ src/Connect4.java
@@ -1,18 +1,45 @@
 import javax.swing.*;
 import java.awt.*;
-
+import java.awt.Graphics;
 
 
 public class Connect4 extends JFrame {
-     
 
-    private static void Connect4(){
-        JFrame frame = new JFrame("Connect 4");
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        frame.setSize(800,600);
+
+    public static void main(String[] args) {
+        new Connect4();
 
     }
+
+    public Connect4(){
+       setTitle("Connect4");
+        setVisible(true);
+       setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(800,700); // Set width and height to 800x800
+
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.black); //Sets Outline color to black
+
+        int x=25, y=50;
+
+        for(x=25; x<945; x=x+110){ //Outer loop draws horizontal ovals
+            for(y=50; y<600; y=y+100){ //inner loop draws vertical ovals
+
+                g.drawOval(x, y, 80, 80); 
+
+            }
+
+
+        }
+
+
+    }
+
+
+
+
 
 
 
