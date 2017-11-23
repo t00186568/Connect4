fecd850c0552ca44a01efc8715afe96202c7f557
t00186568 <kieran.mcclure@students.ittralee.ie>
2017-11-23 09:25:41 +0000

    Begin Making Buttons
    First Button added - will implement ActionListener after all buttons are created

--- src/Connect4.java
+++ src/Connect4.java
@@ -13,21 +13,33 @@ public class Connect4 extends JFrame {
 
     public Connect4(){
        setTitle("Connect4");
-        setVisible(true);
+
        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(800,700); // Set width and height to 800x800
+        setSize(800,750); // Set width and height to 800x800
+        setVisible(true);
+        setLayout(null);
+
+
+    }
+
+    public void buttons(){
+        //Button 1
+        JButton slot1=new JButton("Slot 1"); //declare new button
+        slot1.setEnabled(true); //enable new button
+        add(slot1); //Add new button to jFrame
+        slot1.setBounds(10,610,100,50); // Set new Button Size and Location
 
     }
 
     public void paint(Graphics g) {
-        g.setColor(Color.black); //Sets Outline color to black
+        //g.setColor(Color.black); //Sets Outline color to black
 
         int x=25, y=50;
 
-        for(x=25; x<945; x=x+110){ //Outer loop draws horizontal ovals
-            //if(x/135) = false slot empty -- code for checking if slot is empty
+        for(x=25; x<745; x=x+110){ //Outer loop draws horizontal ovals
+            //if(x/135) = "empty" slot empty -- code for checking if slot is empty
             for(y=50; y<600; y=y+100){ //inner loop draws vertical ovals
-                //if(y/600) = false slot empty
+                //if(y/600) = "empty" slot empty
                 g.drawOval(x, y, 80, 80);
 
             }
@@ -39,7 +51,7 @@ public class Connect4 extends JFrame {
     }
 
 
-//string array - empty, red or blue
+//int array - 0 = empty, 1= red or 2 = blue
 // Counter++ for turn tracking - will modulus to check who's turn it is, red is evens, blue is odds
 // Each button will have a counter to know how many rows up each counter is
 
