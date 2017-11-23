8d1e9a0cc468b2243247dc5c613dc398cb803c5f
t00186568 <kieran.mcclure@students.ittralee.ie>
2017-11-23 13:05:46 +0000

    Buttons Created
    Problems - Buttons don't all appear until hovered over
    Button is recoloring all circles to color instead of just one

--- src/Connect4.java
+++ src/Connect4.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class Connect4 extends JFrame {
@@ -11,25 +13,95 @@ public class Connect4 extends JFrame {
 
     }
 
+    public int counter = 1;
+    public Color color = Color.black;
     public Connect4(){
        setTitle("Connect4");
 
        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(800,750); // Set width and height to 800x800
+        setSize(800,700); // Set width and height to 800x800
+
+        setLayout(new FlowLayout(FlowLayout.LEFT));
         setVisible(true);
-        setLayout(null);
+
+         // Turn counter, odds - blue, evens - red
 
 
-    }
-
-    public void buttons(){
         //Button 1
         JButton slot1=new JButton("Slot 1"); //declare new button
         slot1.setEnabled(true); //enable new button
         add(slot1); //Add new button to jFrame
         slot1.setBounds(10,610,100,50); // Set new Button Size and Location
+        slot1.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+
+                    for(int i = 0; i<7; i++){
+
+                           if(Array.slotStatus[1][i] == 0) {
+                               if (counter % 2 == 1) { //Sets Array entry
+
+                                   Array.slotStatus[1][i] = 1;
+                                   color = Color.red;
+                                   repaint();
+                                    break;
+
+                               }else {
+                                   Array.slotStatus[1][i] = 2;
+
+                                   break;
+                               }
+
+                           }
+
+                       }
+
+                       System.out.print(Array.slotStatus[1][0]);
+                        counter++;
+                }
+            });
+
+
+
+        //Button2
+        JButton slot2=new JButton("Slot 2");
+        slot2.setBounds(120,610,100,50);
+        slot2.setEnabled(true);
+        add(slot2);
+
+        //Button3
+        JButton slot3=new JButton("Slot 3");
+        slot3.setBounds(230,610,100,50);
+        slot3.setEnabled(true);
+        add(slot3);
+
+        //Button4
+        JButton slot4=new JButton("Slot 4");
+        slot4.setBounds(340,610,100,50);
+        slot4.setEnabled(true);
+        add(slot4);
+
+        //Button5
+        JButton slot5=new JButton("Slot 5");
+        slot5.setBounds(450,610,100,50);
+        slot5.setEnabled(true);
+        add(slot5);
+
+        //Button6
+        JButton slot6=new JButton("Slot 6");
+        slot6.setBounds(560,610,100,50);
+        slot6.setEnabled(true);
+        add(slot6);
+
+        //Button2
+        JButton slot7=new JButton("Slot 7");
+        slot7.setBounds(670,610,100,50);
+        slot7.setEnabled(true);
+        add(slot7);
+
 
     }
+
+
 
     public void paint(Graphics g) {
         //g.setColor(Color.black); //Sets Outline color to black
@@ -37,16 +109,21 @@ public class Connect4 extends JFrame {
         int x=25, y=50;
 
         for(x=25; x<745; x=x+110){ //Outer loop draws horizontal ovals
-            //if(x/135) = "empty" slot empty -- code for checking if slot is empty
+
             for(y=50; y<600; y=y+100){ //inner loop draws vertical ovals
-                //if(y/600) = "empty" slot empty
-                g.drawOval(x, y, 80, 80);
 
-            }
-
-
+                    g.setColor(color);
+                    g.drawOval(x, y, 80, 80);
+                }
         }
 
+        for(int i = 0; i<6; i++){
+            if(Array.slotStatus[i][i] == 1){
+                g.setColor(Color.RED);
+                g.fillOval((i*135), (i*100), 80, 80);
+
+            }
+        }
 
     }
 
