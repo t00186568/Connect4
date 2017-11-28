706a8aa181dd9172b818bf09302a957c036fc2a2
t00186568 <kieran.mcclure@students.ittralee.ie>
2017-11-28 13:41:15 +0000

    Moving away from original idea of Repainting
    My original idea of using loops to paint Ovals using the graphics method was creating far too may bugs and was not predictable enough for me to be happy with it. My new idea is to use a GridLayout with JButtons, the buttons will interface with the array and change color accodingly on click using ActionListener

--- src/Array.java
+++ src/Array.java
@@ -1,12 +1,24 @@
+import java.util.Arrays;
+
 public class Array {
 
-    public static boolean[][] slotStatus =  new boolean[6][7];
+    public static int[][] slotStatus =  new int[6][7];
+
+    public static void fillArray(){
+       java.util.Arrays.fill(slotStatus, 0);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.print(slotStatus[3][4]);
+    }
+
 
     /*
     Code to make sure that the array was properly written too
     public static void main(String[] args) {
         boolean slot = slotStatus[4][5];
-        if(slot==false) {
+        if(slot=="empty") {
             System.out.print("yeah");
         }else{
             System.out.print("nope");
--- src/Connect4.java
+++ src/Connect4.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 
 public class Connect4 extends JFrame {
@@ -11,37 +14,17 @@ public class Connect4 extends JFrame {
 
     }
 
-    public Connect4(){
-       setTitle("Connect4");
-        setVisible(true);
-       setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(800,700); // Set width and height to 800x800
+    public int counter = 1; // Turn counter, odds - blue, evens - red Global variable
+    public Color color = Color.black; //sets global color
+    public Connect4() {
+        setTitle("Connect4");
 
-    }
-
-    public void paint(Graphics g) {
-        g.setColor(Color.black); //Sets Outline color to black
-
-        int x=25, y=50;
-
-        for(x=25; x<945; x=x+110){ //Outer loop draws horizontal ovals
-            //if(x/135) = false slot empty -- code for checking if slot is empty
-            for(y=50; y<600; y=y+100){ //inner loop draws vertical ovals
-                //if(y/600) = false slot empty
-                g.drawOval(x, y, 80, 80);
-
-            }
-
-
-        }
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(800, 700); // Set width and height to 800x800
 
 
     }
-
-
-//string array - empty, red or blue
-// Counter++ for turn tracking - will modulus to check who's turn it is, red is evens, blue is odds
-// Each button will have a counter to know how many rows up each counter is
+    
 
 
 
