73436ecba38f40115045054fdf0ba34f2ce210ea
t00186568 <kieran.mcclure@students.ittralee.ie>
2017-11-28 14:04:57 +0000

    Grid of buttons
    For loops for generating buttons created, will implement actionListener for buttons next

--- src/Array.java
+++ src/Array.java
@@ -4,14 +4,7 @@ public class Array {
 
     public static int[][] slotStatus =  new int[6][7];
 
-    public static void fillArray(){
-       java.util.Arrays.fill(slotStatus, 0);
 
-    }
-
-    public static void main(String[] args) {
-        System.out.print(slotStatus[3][4]);
-    }
 
 
     /*
--- src/Connect4.java
+++ src/Connect4.java
@@ -14,17 +14,39 @@ public class Connect4 extends JFrame {
 
     }
 
-    public int counter = 1; // Turn counter, odds - blue, evens - red Global variable
-    public Color color = Color.black; //sets global color
-    public Connect4() {
-        setTitle("Connect4");
 
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(800, 700); // Set width and height to 800x800
+    Connect4() {
+       JFrame connect4;
+       connect4 = new JFrame();
+       connect4.setTitle("Connect4");
+       connect4.setDefaultCloseOperation(EXIT_ON_CLOSE);
+       connect4.setVisible(true);
+       connect4.setSize(800,600);
+        JButton grid[][] = new JButton[6][7];  //Array for drawing JButtons in grid
+        int status[][] = new int[6][7]; //Array for tracking what status of buttons are
+
+       //Begin Buttons
+        for(int i=0; i <6; i++) //For loop for drawing vertical buttons
+            for(int  q=0; q<7; q++){ //For loop for drawing horizontal buttons
+                grid[i][q] = new JButton(String.valueOf(i) + "," + String.valueOf(q)); //Gives buttons their Grid location Numbers -- will remove
+                grid[i][q].setBackground(Color.white);
+                grid[i][q].setOpaque(true);
+                connect4.add(grid[i][q]);
+            }
+
+
+
+
+
+
+
+       connect4.setLayout(new GridLayout(6,7));
+
+
 
 
     }
-    
+
 
 
 
